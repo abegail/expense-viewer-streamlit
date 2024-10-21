2201a28fc323e8c72fbd2dce03a7c174a62e759a
Abegail Pates <abegail.pates@gmail.com>
2024-10-21 12:00:08 +0800

    Change pie chart to buckets

--- app.py
+++ app.py
@@ -10,7 +10,30 @@ category_sum = df.groupby('Category')['Cost'].sum()
 category_sum_df = category_sum.reset_index()
 category_sum_df.columns = ['Category', 'Total']
 
+category_to_bucket = {
+    'Clothing': 'Fixed Costs',
+    'Debt Repayment': 'Fixed Costs',
+    'Eating Out': 'Guilt-free Spending',
+    'Food': 'Fixed Costs',
+    'Giving': 'Guilt-free Spending',
+    'Groceries': 'Fixed Costs',
+    'Housing': 'Fixed Costs',
+    'Meds': 'Fixed Costs',
+    'Pets': 'Fixed Costs',
+    'Subscriptions': 'Fixed Costs',
+    'Transportation': 'Fixed Costs',
+    'Travel': 'Guilt-free Spending',
+    'Wants': 'Guilt-free Spending',
+    'Emergency': 'Emergency Spend',
+    'Taxes': 'Tax',
+}
+
+df['Buckets'] = df['Category'].map(category_to_bucket)
+aggregated_df = df.groupby('Buckets').sum().reset_index()
+
+fig = px.pie(category_sum_df, values='Total', names='Buckets')
+
 st.dataframe(category_sum_df)
 
-fig = px.pie(category_sum_df, values='Total', names='Category')
-st.plotly_chart(fig)
+# fig = px.pie(category_sum_df, values='Total', names='Category')
+# st.plotly_chart(fig)
