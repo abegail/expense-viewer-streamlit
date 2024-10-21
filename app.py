572a5c99092b729c82cf394191e8f327db51a6df
Abegail Pates <abegail.pates@gmail.com>
2024-10-21 10:52:15 +0800

    Display by category

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("Expense Viewer")
 
 df = pd.read_csv('ExpenseExport.csv')
-st.dataframe(df)
+
+category_sum = df.groupby('Category')['Cost'].sum()
+category_sum_df = category_sum.reset_index()
+category_sum_df.columns = ['Category', 'Total']
+
+st.dataframe(category_sum_df)
