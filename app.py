3f50510c059946aa8e221c5638ece3443d438886
Abegail Pates <abegail.pates@gmail.com>
2024-10-21 10:38:55 +0800

    Remove ability to upload csv. Read local file instead.

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ import pandas as pd
 
 st.title("Expense Viewer")
 
-uploaded_csv = st.file_uploader("Upload CSV file")
-
-if (uploaded_csv != None):
-    df = pd.read_csv(uploaded_csv)
-    st.dataframe(df)
+df = pd.read_csv('ExpenseExport.csv')
+st.dataframe(df)
